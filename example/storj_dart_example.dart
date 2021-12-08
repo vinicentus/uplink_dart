72a7aaa01fffb992ff42047d3035f82148055ba3
Vincent Långström <vincent.langstrom@gmail.com>
2021-12-08 16:58:53 +0200

    add example
    import .so at runtime
    move generated bindings

--- example/storj_dart_example.dart
+++ example/storj_dart_example.dart
@@ -1,6 +1,12 @@
+import 'package:storj_dart/generated_bindings.dart';
 import 'package:storj_dart/storj_dart.dart';
+import 'dart:ffi' as ffi;
 
 void main() {
   var awesome = Awesome();
   print('awesome: ${awesome.isAwesome}');
+
+  var lib = NativeLibrary(ffi.DynamicLibrary.open('libuplinkc.so'));
+
+  // UplinkAccessResult access = lib.uplink_parse_access(accessString);
 }

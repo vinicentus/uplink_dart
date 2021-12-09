0aeefe15a9a1a480717d5ff61995c34eaabc20f3
Vincent Långström <vincent.langstrom@gmail.com>
2021-12-09 10:53:24 +0200

    error string example

--- example/storj_dart_example.dart
+++ example/storj_dart_example.dart
@@ -1,12 +1,21 @@
 import 'package:storj_dart/generated_bindings.dart';
 import 'package:storj_dart/storj_dart.dart';
-import 'dart:ffi' as ffi;
+import 'dart:ffi';
+import 'package:ffi/ffi.dart';
 
 void main() {
   var awesome = Awesome();
   print('awesome: ${awesome.isAwesome}');
 
-  var lib = NativeLibrary(ffi.DynamicLibrary.open('libuplinkc.so'));
+  var lib = NativeLibrary(DynamicLibrary.open('libuplinkc.so'));
 
-  // UplinkAccessResult access = lib.uplink_parse_access(accessString);
+  var dartAccessString = 'example';
+  var charPointer = dartAccessString.toNativeUtf8();
+  var intPointer = charPointer.cast<Int8>();
+  UplinkAccessResult accessResult = lib.uplink_parse_access(intPointer);
+  Pointer<UplinkError> errorPointer = accessResult.error;
+  UplinkError errorStruct = errorPointer.ref;
+  print(errorStruct.code);
+  var errorMessage = errorStruct.message.cast<Utf8>().toDartString();
+  print(errorMessage);
 }

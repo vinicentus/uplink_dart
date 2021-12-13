959ca22b15b36314ea4001975189fb7a20b965c1
Vincent Långström <vincent.langstrom@gmail.com>
2021-12-13 10:27:26 +0200

    basic documentation

--- lib/storj_dart.dart
+++ lib/storj_dart.dart
@@ -1,4 +1,5 @@
-/// Support for doing something awesome.
+/// A dart wrapper for the storj uplink go package.
+/// Uses uplink-c (c bindings fro the go package) under the hood.
 ///
 /// More dartdocs go here.
 library storj_dart;
